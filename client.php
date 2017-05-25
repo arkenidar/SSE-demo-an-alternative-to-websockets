5976ae1bd53d03ec980552c50108c2e91dd2907c
Dario Cangialosi <dario.cangialosi@gmail.com>
2017-05-25 12:37:04 +0200

    default cid when missing. send button(more visible). open clients in new tab from links.

--- client.php
+++ client.php
@@ -3,14 +3,14 @@ function send(){
 var tts = document.all.tts.value
 fetch('insert.php?msg='+tts+'&cids=1,2')
 }
-const source = new EventSource('sse.php?cid='+'<?=$_REQUEST["cid"]?>')
+const source = new EventSource('sse.php?cid='+'<?=isset($_REQUEST["cid"])?(int)$_REQUEST["cid"]:1?>')
 source.onmessage = function (event) {
     //alert(event.data);
     document.title = event.data
     document.all.out.innerHTML += event.data+'<br>'
 }
 </script>
-<a onclick=send()>send</a>
+<button onclick=send()>send</button>
 <input id=tts>
-<a href=client.php?cid=1>1</a> <a href=client.php?cid=2>2</a>
+<a href=client.php?cid=1 target=_blank>1</a> <a href=client.php?cid=2 target=_blank>2</a>
 <div id=out></div>
