64934391e316022dfe313615490c7245175aaf7f
Dario Cangialosi <dario.cangialosi@gmail.com>
2017-05-25 12:40:57 +0200

    shows cid.

--- client.php
+++ client.php
@@ -1,16 +1,17 @@
+<?php $cid = isset($_REQUEST["cid"])?(int)$_REQUEST["cid"]:1; ?>
 <script>
 function send(){
 var tts = document.all.tts.value
 fetch('insert.php?msg='+tts+'&cids=1,2')
 }
-const source = new EventSource('sse.php?cid='+'<?=isset($_REQUEST["cid"])?(int)$_REQUEST["cid"]:1?>')
+const source = new EventSource('sse.php?cid='+'<?=$cid?>')
 source.onmessage = function (event) {
     //alert(event.data);
     document.title = event.data
     document.all.out.innerHTML += event.data+'<br>'
 }
 </script>
-<button onclick=send()>send</button>
+cid: <?=$cid?> <button onclick=send()>send</button>
 <input id=tts>
 <a href=client.php?cid=1 target=_blank>1</a> <a href=client.php?cid=2 target=_blank>2</a>
 <div id=out></div>
