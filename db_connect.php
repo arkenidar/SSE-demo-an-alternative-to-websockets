727f2691a9cf6df4f91415ef5a22fbaf600f9d0f
Dario Cangialosi <dario.cangialosi@gmail.com>
2018-09-03 08:50:50 +0000

    gitignore

--- db_connect.php
+++ db_connect.php
@@ -11,6 +11,7 @@ function db_connect(){
     $db_username = $db_config['user'];
     $db_password = $db_config['password'];
     $db = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8", $db_username, $db_password);
+    assert($db!=null);
     return $db;
 }
 
